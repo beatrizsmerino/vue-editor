790c5479630530a168f7023437646c98241f6a3b
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-25 02:08:38 +0100

    chore(eslint): update 'eslint' configuration for change extends and plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,20 @@ module.exports = {
 	env: {
 		node: true
 	},
-	extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
 	parserOptions: {
 		parser: '@babel/eslint-parser'
 	},
+	extends: [
+		'prettier',
+		'eslint:recommended',
+		'plugin:vue/recommended',
+		'plugin:json/recommended'
+	],
+	plugins: [
+		'vue',
+		'json',
+		'prettier'
+	],
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
