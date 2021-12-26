4207e5add8557cd1a8e527db1f91edc6a9b9f884
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-27 00:30:12 +0100

    feat(editor-json): create the 'EditorJSON' component and import it into the 'Editor' view

---
+++ src/components/EditorJSON.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="editor-json">
+		<h2>
+			Vue JSON editor
+		</h2>
+		<vue-json-editor
+			v-model="json"
+			:show-btns="true"
+			:expanded-on-start="true"
+			@json-change="onJsonChange"
+		/>
+	</div>
+</template>
+
+<script>
+	import vueJsonEditor from 'vue-json-editor';
+
+	export default {
+		name: 'Editor',
+		components: {
+			vueJsonEditor
+		},
+		data() {
+			return {
+				json: {
+					msg: 'demo of jsoneditor'
+				}
+			};
+		},
+		methods: {
+			onJsonChange(value) {
+				console.log('value:', value);
+			}
+		}
+	};
+</script>
--- src/views/Editors.vue
+++ src/views/Editors.vue
@@ -1,11 +1,19 @@
 <template>
 	<div class="editors">
 		<h1>Editors page</h1>
+		<EditorJSON />
 	</div>
 </template>
 
 <script>
+
+	// @ is an alias to /src
+	import EditorJSON from '@/components/EditorJSON.vue';
+
 	export default {
-		name: 'Editors'
+		name: 'Editors',
+		components: {
+			EditorJSON
+		}
 	};
 </script>
