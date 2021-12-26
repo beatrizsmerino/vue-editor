181d2b9b4b446f5aeb9bfaa9341f3ffc815fa1cc
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-12-26 23:41:12 +0100

    refactor(nav): updated html structure and styles of the 'Nav' component
    - Add 'ul' tag and moved each link to each 'li' tag

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,23 @@
 <template>
 	<div class="nav">
-		<router-link
-			to="/"
-			class="nav__link"
-		>
-			Home
-		</router-link>
-		|
-		<router-link
-			to="/about"
-			class="nav__link"
-		>
-			About
-		</router-link>
+		<ul class="nav__list">
+			<li class="nav__item">
+				<router-link
+					to="/"
+					class="nav__link"
+				>
+					Home
+				</router-link>
+			</li>
+			<li class="nav__item">
+				<router-link
+					to="/about"
+					class="nav__link"
+				>
+					About
+				</router-link>
+			</li>
+		</ul>
 	</div>
 </template>
 
@@ -24,7 +29,30 @@
 
 <style lang="scss" scoped>
 	.nav {
+		display: flex;
+		justify-content: center;
 		padding: 30px;
+
+		&__list {
+			display: flex;
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+
+		&__item {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			&:not(:last-child) {
+				&:after {
+					content: '|';
+					display: inline-block;
+					padding: 0 1rem;
+				}
+			}
+		}
 
 		&__link {
 			color: $color-brand-2;
