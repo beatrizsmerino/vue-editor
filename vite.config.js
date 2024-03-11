6440b640f1fc1bc2515b1c61fee380a1256da851
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-03-11 20:49:18 +0100

    fix(config): use `fileURLToPath` for add the alias `@`
    Steps:
    - In the `vite.config.js` file:
    1. Import the `fileURLToPath` and `URL` functions from the `url` package instead import `resolve` and `dirname` functions from the `path` package.
    2. Use `fileURLToPath(new URL("./src", import.meta.url))` instead of `resolve(dirname(new URL(import.meta.url).pathname), "./src")`.
    
    Reason:
    - The `path` package is mostly compatible between operating systems.
    But the `pathname` property of the `URL` object does not correctly interpret the drive letter and colon (e.g. `/C:/path/` instead of `C:/path`), which can result in incorrect paths on Windows systems.
    - Because of how directory slashes are handled on Windows, it is safer to use the `fileURLToPath` utility function provided by `node`, to correctly handle the conversion of a URL into a local file path.
    This function automatically takes into account the different path separators used on Windows (`/`) and on POSIX (portable operating system interface for UNIX) compliant systems such as macOS and Linux (`/`).
    This ensures that your code runs consistently without any unexpected behavior on all operating systems supported by `node`, including Windows, macOS and Linux, when handling URL derived file paths.
    
    References:
    - https://jasonsteinshouer.com/vite-migration-notes#vite-config
    - https://medium.com/nerd-for-tech/from-vue-cli-to-vitejs-648d2f5e031d

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { resolve, dirname } from "path";
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 
@@ -9,7 +9,7 @@ export default defineConfig({
 	],
 	"resolve": {
 		"alias": {
-			"@": resolve(dirname(new URL(import.meta.url).pathname), "./src"),
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
 	},
 });
