25f0685aa37a28e28c1bf76b1a3fe377f37fbb60
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-03-11 20:32:40 +0100

    feat(config): add the `@` as alias of the `src` folder
    Steps:
    - Update the `vite.config.js` file:
    1. Import the `path` module with `require` and store it in a constant.
    2. Inside the `defineConfig` block, add a new `resolve` property if it doesn't exist yet.
    3. Within the `resolve` object, insert an `alias` key.
    4. Define the alias for the `src` directory by setting the `'@'` key to `path.resolve(__dirname, './src')`.
    This indicates that any import statement that begins with `@/` should be considered as starting from the `src` directory.
    
    Reason:
    - To make everything work as with the `@vue/cli` packages, we will have to add the `@` as alias to the `src` folder.
    - The `path` module of `node` is necessary to ensure that file paths are handled correctly on different operating systems. Using `path.resolve()`, relative paths are converted to absolute paths.
    - The `@` alias is set to point to the `src` directory. This simplifies the process of importing files, as you can now use `@` to refer to the `src` directory, avoiding long relative paths (`../../../../../`).
    - By following these steps, `import` statements will be cleaner and the code base easier to maintain, especially as the project grows in size and complexity.
    The use of `@` to represent the `src` directory is a common practice in Vue projects, making code more readable and imports more concise.
    
    References:
    - https://vueschool.io/articles/vuejs-tutorials/how-to-migrate-from-vue-cli-to-vite/

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 
+const path = require("path");
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	"plugins": [
 		vue(),
 	],
+	"resolve": {
+		"alias": {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
