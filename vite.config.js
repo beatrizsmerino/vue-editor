41c082712e92f7bee6c352ab51b5fd33c34e6bad
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-03-11 20:37:49 +0100

    fix(config): use `ESModules` instead `CommonJS` for add the alias `@`
    Steps:
    - In the `vite.config.js` file:
    1. Replaced the `require()` statement with an `import` statement to get the `path` module.
    2. Replaced `__dirname` with `path.dirname(new URL(import.meta.url).pathname)` to resolve the path to the `src` directory.
    
    Reason:
    - Vite is a modern package, which requires the use of `ESModules` over `CommonJS` syntax.
    So since `require` or `__dirname` are not available in the `ESModules` context they must be substituted.
    1. To import packages as you would with `require` in a `node` environment, you must use `import` for module management.
    2. To define the `alias` in the `vite.config.js` file as you would with the `__dirname` global variable in a `node` environment, you can use the `URL` interface to get the absolute path to the current directory.
    In this case, `import.meta.url` will give you the url of the current module, and `path.dirname` the directory of that module.
    
    References:
    - https://vueschool.io/articles/vuejs-tutorials/how-to-migrate-from-vue-cli-to-vite/

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
-
-const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
 	],
 	"resolve": {
 		"alias": {
-			"@": path.resolve(__dirname, "./src"),
+			"@": path.resolve(path.dirname(new URL(import.meta.url).pathname), "./src"),
 		},
 	},
 });
