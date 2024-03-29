21e4532cfd1b29ae90debe776e64eae3ee4c4db8
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-03-29 18:51:29 +0100

    refactor(page): create `PageHeader` component
    Steps:
    - Create `PageHeader.vue` file.
    - Move the code about the `header` html tag from `App.vue` file to `PageHeader.vue` file.
    Add to the `script` tag the `import` of the components used, the `components` list used and the component `name`.

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<header class="page__header">
+		<PageNav />
+	</header>
+</template>
+
+<script>
+	import PageNav from "@/components/PageNav.vue";
+	export default {
+		"name": "PageHeader",
+		"components": {
+			PageNav,
+		},
+	};
+</script>
