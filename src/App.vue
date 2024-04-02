95d1d30a5c952e54793db90a59a7b1b23e08954d
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-04-03 01:48:55 +0200

    refactor(LayoutDefault): remove unnecessary comment

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,6 @@
 </script>
 
 <style lang="scss">
-	// PARTIALS SASS
-	// =================================================
-	// Pattern 7-1
-	// https://sass-guidelin.es/#the-7-1-pattern
-	// 1. ABSTRACTS (settings that not generates code)
-	// 2. BASE (standard styles for commonly used HTML elements)
-	// 3. COMPONENTS (reusables micro modules of the widgets)
-	// 4. LAYOUTS (global wireframe with the main parts)
-	// 5. PAGES (styles for each page)
-	// 6. THEMES (change the state of colors dark or light)
-	// 7. VENDORS (external css code of the libraries and frameworks)
-
-	// BASE
-	//----------------------------------------------------------------------
 	@import "@/assets/scss/base/base-reset";
 	@import "@/assets/scss/base/base-global";
 </style>
