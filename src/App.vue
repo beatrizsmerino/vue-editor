47c8a3f38938a52c2bbba3438bb91a2134298b82
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-04-03 01:05:52 +0200

    feat(LayoutDefault): move content of the `template` and `script` tags from `App` to the layout

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
 <template>
-	<div id="app">
-		<PageHeader />
-		<main class="page__main">
-			<router-view />
-		</main>
-	</div>
+	<div />
 </template>
 
 <script>
-	import PageHeader from "@/components/Page/PageHeader.vue";
 	export default {
 		"name": "App",
-		"components": {
-			PageHeader,
-		},
-		"watch": {
-			"$route": {
-				handler(to, from) {
-					const html = document.getElementsByTagName("html")[0];
-					const body = document.getElementsByTagName("body")[0];
-
-					from?.name && html.classList.remove("page", `page-${from.name.toLowerCase()}`);
-					to?.name && html.classList.add("page", `page-${to.name.toLowerCase()}`);
-
-					body.classList.add("page__body");
-				},
-				"immediate": true,
-			},
-		},
 	};
 </script>
 
--- src/layouts/LayoutDefault.vue
+++ src/layouts/LayoutDefault.vue
@@ -1,12 +1,34 @@
 <template>
-	<div>
-		<slot />
+	<div id="app">
+		<PageHeader />
+		<main class="page__main">
+			<router-view />
+		</main>
 	</div>
 </template>
 
 <script>
+	import PageHeader from "@/components/Page/PageHeader.vue";
+
 	export default {
 		"name": "LayoutDefault",
+		"components": {
+			PageHeader,
+		},
+		"watch": {
+			"$route": {
+				handler(to, from) {
+					const html = document.getElementsByTagName("html")[0];
+					const body = document.getElementsByTagName("body")[0];
+
+					from?.name && html.classList.remove("page", `page-${from.name.toLowerCase()}`);
+					to?.name && html.classList.add("page", `page-${to.name.toLowerCase()}`);
+
+					body.classList.add("page__body");
+				},
+				"immediate": true,
+			},
+		},
 	};
 </script>
 
