7c01bc820b8ffd3af9918ea3f19171167ac0391a
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-04-04 14:43:27 +0200

    feat(assets): add and import css styles from `vite-project`

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./assets/css/main.css";
 
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 Vue.component("DefaultLayout", DefaultLayout);
