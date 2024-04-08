3a5826d665e528b5f63df85d40e5110cdbfe7619
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2024-04-08 03:59:28 +0200

    fix(TheWelcome): continues the extra spaces after the `a` tags
    Error:
    - The code is incorrectly formatted by `prettier` and `eslint`.
    
    Solution:
    - Import `ref` from `vue`, because this component uses Composition API.
    - Move the html of each `WelcomeItem` to the `data` constant.
    - Use the `v-html` directive to print the html as a string and prevent it from being formatted.

--- src/components/Hello/TheWelcome.vue
+++ src/components/Hello/TheWelcome.vue
@@ -1,5 +1,75 @@
 <script setup>
+	import { ref } from "vue";
 	import WelcomeItem from "@/components/Hello/WelcomeItem.vue";
+
+	const data = ref([
+		{
+			"title": "Documentation",
+			"icon": "documentation",
+			"text": `
+				Vue’s
+				<a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+				provides you with all information you need to get started.
+			`,
+		},
+		{
+			"title": "Tooling",
+			"icon": "tooling",
+			"text": `
+				This project is served and bundled with
+				<a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
+				recommended IDE setup is
+				<a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
+				<a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
+				you need to test your components and web pages, check out
+				<a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
+				<a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+				>Cypress Component Testing</a
+				>.
+				<br />
+				More instructions are available in <code>README.md</code>.
+			`,
+		},
+		{
+			"title": "Ecosystem",
+			"icon": "ecosystem",
+			"text": `
+				Get official tools and libraries for your project:
+				<a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
+				<a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
+				<a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
+				<a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
+				you need more resources, we suggest paying
+				<a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
+				a visit.
+			`,
+		},
+		{
+			"title": "Community",
+			"icon": "community",
+			"text": `
+				Got stuck? Ask your question on
+				<a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
+				Discord server, or
+				<a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
+				>StackOverflow</a
+				>. You should also subscribe to
+				<a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
+				the official
+				<a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
+				twitter account for latest news in the Vue world.
+			`,
+		},
+		{
+			"title": "Support Vue",
+			"icon": "support",
+			"text": `
+				As an independent project, Vue relies on community backing for its sustainability. You can help
+				us by
+				<a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+			`,
+		},
+	]);
 </script>
 
 <template>
@@ -7,213 +77,71 @@
 		<WelcomeItem>
 			<template #icon>
 				<UIIcon
-					name="documentation"
-					aria-label="Documentation"
-					title="Documentation"
+					:name="data[0].icon"
+					:aria-label="data[0].title"
+					:title="data[0].title"
 				/>
 			</template>
 			<template #heading>
-				Documentation
+				{{ data[0].title }}
 			</template>
-			<p>
-				Vue’s
-				<a
-					href="https://vuejs.org/"
-					target="_blank"
-					rel="noopener"
-				>
-					official documentation
-				</a>
-				provides you with all information you need to get started.
-			</p>
+			<p v-html="data[0].text" />
 		</WelcomeItem>
 
 		<WelcomeItem>
 			<template #icon>
 				<UIIcon
-					name="tooling"
-					aria-label="Tooling"
-					title="Tooling"
+					:name="data[1].icon"
+					:aria-label="data[1].title"
+					:title="data[1].title"
 				/>
 			</template>
 			<template #heading>
-				Tooling
+				{{ data[1].title }}
 			</template>
-			<p>
-				This project is served and bundled with
-				<a
-					href="https://vitejs.dev/guide/features.html"
-					target="_blank"
-					rel="noopener"
-				>
-					Vite
-				</a>
-				. The recommended IDE setup is
-				<a
-					href="https://code.visualstudio.com/"
-					target="_blank"
-					rel="noopener"
-				>
-					VSCode
-				</a>
-				+
-				<a
-					href="https://github.com/johnsoncodehk/volar"
-					target="_blank"
-					rel="noopener"
-				>
-					Volar
-				</a>
-				. If you need to test your components and web pages, check out
-				<a
-					href="https://www.cypress.io/"
-					target="_blank"
-					rel="noopener"
-				>
-					Cypress
-				</a>
-				and
-				<a
-					href="https://on.cypress.io/component"
-					target="_blank"
-					rel="noopener"
-				>
-					Cypress Component Testing
-				</a>
-				.
-				<br >
-				More instructions are available in
-				<code>README.md</code>
-				.
-			</p>
+			<p v-html="data[1].text" />
 		</WelcomeItem>
 
 		<WelcomeItem>
 			<template #icon>
 				<UIIcon
-					name="ecosystem"
-					aria-label="Ecosystem"
-					title="Ecosystem"
+					:name="data[2].icon"
+					:aria-label="data[2].title"
+					:title="data[2].title"
 				/>
 			</template>
 			<template #heading>
-				Ecosystem
+				{{ data[2].title }}
 			</template>
-			<p>
-				Get official tools and libraries for your project:
-				<a
-					href="https://pinia.vuejs.org/"
-					target="_blank"
-					rel="noopener"
-				>
-					Pinia
-				</a>
-				,
-				<a
-					href="https://router.vuejs.org/"
-					target="_blank"
-					rel="noopener"
-				>
-					Vue Router
-				</a>
-				,
-				<a
-					href="https://test-utils.vuejs.org/"
-					target="_blank"
-					rel="noopener"
-				>
-					Vue Test Utils
-				</a>
-				, and
-				<a
-					href="https://github.com/vuejs/devtools"
-					target="_blank"
-					rel="noopener"
-				>
-					Vue Dev Tools
-				</a>
-				. If you need more resources, we suggest paying
-				<a
-					href="https://github.com/vuejs/awesome-vue"
-					target="_blank"
-					rel="noopener"
-				>
-					Awesome Vue
-				</a>
-				a visit.
-			</p>
+			<p v-html="data[2].text" />
 		</WelcomeItem>
 
 		<WelcomeItem>
 			<template #icon>
 				<UIIcon
-					name="community"
-					aria-label="Community"
-					title="Community"
+					:name="data[3].icon"
+					:aria-label="data[3].title"
+					:title="data[3].title"
 				/>
 			</template>
 			<template #heading>
-				Community
+				{{ data[3].title }}
 			</template>
-			<p>
-				Got stuck? Ask your question on
-				<a
-					href="https://chat.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					Vue Land
-				</a>
-				, our official Discord server, or
-				<a
-					href="https://stackoverflow.com/questions/tagged/vue.js"
-					target="_blank"
-					rel="noopener"
-				>
-					StackOverflow
-				</a>
-				. You should also subscribe to
-				<a
-					href="https://news.vuejs.org"
-					target="_blank"
-					rel="noopener"
-				>
-					our mailing list
-				</a>
-				and follow the official
-				<a
-					href="https://twitter.com/vuejs"
-					target="_blank"
-					rel="noopener"
-				>
-					@vuejs
-				</a>
-				twitter account for latest news in the Vue world.
-			</p>
+			<p v-html="data[3].text" />
 		</WelcomeItem>
 
 		<WelcomeItem>
 			<template #icon>
 				<UIIcon
-					name="support"
-					aria-label="Support"
-					title="Support"
+					:name="data[4].icon"
+					:aria-label="data[4].title"
+					:title="data[4].title"
 				/>
 			</template>
 			<template #heading>
-				Support Vue
+				{{ data[4].title }}
 			</template>
-			<p>
-				As an independent project, Vue relies on community backing for its sustainability. You can help us by
-				<a
-					href="https://vuejs.org/sponsor/"
-					target="_blank"
-					rel="noopener"
-				>
-					becoming a sponsor
-				</a>
-				.
-			</p>
+			<p v-html="data[4].text" />
 		</WelcomeItem>
 	</div>
 </template>
